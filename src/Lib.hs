6704f23f7e7eb7beb1d2f098c847b227b18c7b28
fedecotens <fcotens@frba.utn.edu.ar>
2025-04-29 15:49:42 -0300

    terminado

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Lib () where
 
 import Text.Show.Functions ()
@@ -7,14 +8,13 @@ doble x = x * 2
 
 data Personaje = Personaje{
     nombre :: String,
-    poderBasico :: String,
-    superPoder :: String,
+    poderBasico :: Personaje -> Personaje,
+    superPoder :: Personaje -> Personaje,
     superPoderActivo :: Bool,
     cantidadDeVida :: Int
-} deriving (Show, Eq)
+} deriving (Show)
 
 -- Funciones para usar
-
 sacarVida :: Personaje -> Int -> Personaje
 sacarVida unPersonaje vida
     | vida >= cantidadDeVida unPersonaje = unPersonaje { cantidadDeVida = 0 }
@@ -26,36 +26,61 @@ sumarVida unPersonaje vida = unPersonaje {cantidadDeVida = cantidadDeVida unPers
 mitadVida :: Personaje -> Personaje
 mitadVida unPersonaje = unPersonaje{cantidadDeVida = div (cantidadDeVida unPersonaje) 2}
 
--- Poderes
+type Personajes = [Personaje]
 
+estaEnLasUltimas :: Personaje -> Bool
+estaEnLasUltimas unPersonaje = cantidadDeVida unPersonaje < 800
+
+
+-- Poderes
 bolaEspinosa :: Personaje -> Personaje
 bolaEspinosa unPersonaje = sacarVida unPersonaje 1000
   
-lluviaDeTuercas :: Personaje -> String -> Personaje -> Personaje
-lluviaDeTuercas unPersonaje tipo otroPersonaje | tipo == "sanadoras" = sumarVida otroPersonaje 800
-                                                 | tipo  == "dañinas" = mitadVida otroPersonaje
-                                                 | otherwise = otroPersonaje                                                 
+lluviaDeTuercas :: String -> Personaje -> Personaje
+lluviaDeTuercas tipo unPersonaje | tipo == "sanadoras" = sumarVida unPersonaje 800
+                                 | tipo  == "dañinas" = mitadVida unPersonaje
+                                 | otherwise = unPersonaje                                                 
                                             
-granadaDeEspinas :: Int -> Personaje -> Personaje ->Personaje
-granadaDeEspinas radio unPersonaje otroPersonaje | radio > 3 && cantidadDeVida otroPersonaje < 800 = (unPersonaje {nombre = nombre unPersonaje ++ "Espinas estuvo aqui"  ,superPoderActivo = False, cantidadDeVida = 0})
-                                                   | radio  > 3 = (unPersonaje {nombre = nombre unPersonaje ++ "Espinas estuvo aqui" })
-                                                   | otherwise = bolaEspinosa unPersonaje
+granadaDeEspinas :: Int -> Personaje ->Personaje
+granadaDeEspinas radio unPersonaje | radio > 3 && cantidadDeVida unPersonaje < 800 = unPersonaje {nombre = nombre unPersonaje ++ "Espinas estuvo aqui", superPoderActivo = False, cantidadDeVida = 0}
+                                   | radio  > 3 = unPersonaje {nombre = nombre unPersonaje ++ "Espinas estuvo aqui" }
+                                   | otherwise = bolaEspinosa unPersonaje
 
 torretaCurativa :: Personaje -> Personaje
 torretaCurativa unPersonaje = unPersonaje {superPoderActivo = True, cantidadDeVida = cantidadDeVida $ sumarVida unPersonaje 100}
 
 
+
+
 -- Reportes
+atacarConElPoderEspecial :: Personaje -> Personaje -> Personaje
+atacarConElPoderEspecial unPersonaje elContrincante
+    | superPoderActivo unPersonaje = (poderBasico unPersonaje).(superPoder unPersonaje) $ elContrincante
+    | otherwise = elContrincante
+
+quienesEstanEnLasUltimas :: Personajes -> [String]
+quienesEstanEnLasUltimas listaPersonajes =  map (nombre) $ filter (estaEnLasUltimas) listaPersonajes
+
+
 
 -- Modelos de personajes
 espina :: Personaje
-espina = Personaje { -- falta radio
+espina = Personaje {
     nombre = "Espina",
-    poderBasico = "Bola Espinosa",
-    superPoder = "Granada de Espinas",
+    poderBasico = (bolaEspinosa),
+    superPoder = (granadaDeEspinas 5),
     superPoderActivo = True,
     cantidadDeVida = 4800
 }
 
+pamela :: Personaje
+pamela = Personaje {
+    nombre = "Pamela",
+    poderBasico = (lluviaDeTuercas "sanadoras"),
+    superPoder = (torretaCurativa),
+    superPoderActivo = False,
+    cantidadDeVida = 9600
+}
 
-
+todosLosPersonajes :: Personajes
+todosLosPersonajes = [pamela, espina]
